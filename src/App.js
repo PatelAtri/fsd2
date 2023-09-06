5543e44a8c2acdb15828f27b631a656abf0eb1a1
PatelAtri <aspatel5123@gmail.com>
2023-09-06 14:34:52 +0530

    react first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Two from './Two'
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Two/>
+    </div>
+  );
+}
+
+export default App;
